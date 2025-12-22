5196703846e69c9184b5b9bc7144009ecbd25a1e
mikaT <m.tores63@outlook.fr>
2025-12-22 13:34:41 +0100

    ADD : back button in partial

--- connexion.php
+++ connexion.php
@@ -5,16 +5,15 @@ require_once "partials/header.php";
 
 
 <main class="bg-main-color min-h-svh flex flex-col items-center justify-center overflow-hidden">
+    <?php require_once "partials/back_button.php" ?>
+
     <div class="w-[80%] flex-1 flex flex-col items-center justify-around">
         <?php require_once "partials/logo.php" ?>
 
         <form class="flex flex-col items-center gap-6" action="pseuConnexion.php" method="POST">
-
             <div class="flex flex-col items-center w-full">
                 <input class=" bg-white placeholder-black text-center" type="text" minlength="3" maxlength="30" placeHolder="Entrez votre pseudo">
             </div>
-
-
 
             <div class="flex flex-col items-center">
                 <button type="submit" class="text_font bg_button_color w-32 p-2 rounded-md flex items-center justify-center cursor-pointer">Valider</button>
---
+++ partials/back_button.php
@@ -0,0 +1,3 @@
+ <div class="w-full p-2">
+     <a href="index.php" aria-label="Retourner à la page précédente"><img class="w-10" src="img/back_button.png" alt="bouton retour"></a>
+ </div>
