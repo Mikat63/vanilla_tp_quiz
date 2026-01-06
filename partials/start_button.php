a1b2de4d4d5df2dfe0ee2a6f84723fcd336cdb06
mikaT <m.tores63@outlook.fr>
2026-01-06 14:28:35 +0100

    completed : score.php with querys for show best score, compare if better thann bdd and update or create a new in bdd

--- partials/start_button.php
+++ partials/start_button.php
@@ -1,6 +1,6 @@
-<div class="w-full flex flex-col items-center">
+<div class="w-full h-auto flex flex-col items-center">
     <a tabindex="0" href="<?= $buttonLink ?>" aria-label="<?= $ariaDescription ?>"
-        class="w-auto p-2 flex items-center justify-center font-[Inter] text-base text-white rounded-md cursor-pointer bg-linear-to-b from-cyan-300 to-blue-800 transition duration-300 hover:from-blue-800 hover:to-cyan-300 hover:brightness-110 focus:from-blue-800 focus:to-cyan-300 focus:brightness-110 lg:text-xl">
+        class="w-auto p-2 flex items-center justify-center font-[Inter] text-base text-white rounded-md cursor-pointer bg-linear-to-b from-cyan-300 to-blue-800 transition duration-300 hover:from-blue-800 hover:to-cyan-300 hover:brightness-110 focus:from-blue-800 focus:to-cyan-300 focus:brightness-110 lg:text-xl whitespace-nowrap">
         <?= $textButton ?>
     </a>
 </div>
---
+++ score_player.php
@@ -0,0 +1,115 @@
+<?php
+session_start();
+$title = "Score";
+require_once "partials/page_infos.php";
+require_once "process/db_connect.php";
+
+var_dump($_SESSION);
+
+// query for the  best score
+$request = $db->prepare(
+    'SELECT 
+                            user_score
+                        FROM
+                            users_themes
+                        WHERE 
+                            id_user = :player_id AND id_theme = :theme_id'
+);
+
+$request->execute([
+    'player_id' => $_SESSION['user_id'],
+    'theme_id' => $_SESSION['theme']['id']
+]);
+
+$userScore = $request->fetch();
+
+// Save score: INSERT if new, UPDATE if betterif (!$userScore) {
+    $request = $db->prepare(
+        'INSERT INTO 
+                users_themes(id_user,id_theme,user_score)
+         VALUES
+                (:user_id,
+                 :theme_id,
+                 :score_user
+                 )'
+    );
+
+    $request->execute([
+        'user_id' => $_SESSION['user_id'],
+        'theme_id' => $_SESSION['theme']['id'],
+        'score_user' => $_SESSION['score']
+    ]);
+} else if ($_SESSION['score'] > $userScore['user_score']) {
+    $request = $db->prepare(
+        'UPDATE 
+                users_themes
+            SET
+                user_score = :score
+            WHERE 
+                id_user = :player_id AND id_theme = :theme_id'
+    );
+
+    $request->execute([
+        'score' => $_SESSION['score'],
+        'player_id' => $_SESSION['user_id'],
+        'theme_id' => $_SESSION['theme']['id']
+    ]);
+};
+
+// Re-fetch thescore query for best score 
+$request = $db->prepare(
+    'SELECT 
+        user_score
+    FROM
+        users_themes
+    WHERE 
+        id_user = :player_id AND id_theme = :theme_id'
+);
+
+$request->execute([
+    'player_id' => $_SESSION['user_id'],
+    'theme_id' => $_SESSION['theme']['id']
+]);
+
+$userScore = $request->fetch();
+?>
+
+
+<main class="min-h-svh flex flex-col items-center justify-center">
+    <!-- main container -->
+    <div class="w-[80%] flex-1 flex flex-col items-center justify-between pb-8">
+        <?php require_once "partials/logo.php";  ?>
+
+        <div class="w-full h-auto flex flex-col gap-4 ">
+            <div class=" w-full h-auto flex flex-col items-center">
+                <h2 class="font-[Manrope] text-[24px] font-bold text-white">MEILLEUR SCORE</h2>
+                <p class="font-[Manrope] text-[20px] font-semibold text-yellow-400"><?= $userScore['user_score'] ?></p>
+            </div>
+
+            <div class=" w-full h-auto flex flex-col items-center">
+                <h2 class="font-[Manrope] text-[24px] font-bold text-white"> SCORE</h2>
+                <p class="font-[Manrope] text-[20px] font-semibold text-yellow-400"><?= $_SESSION['score'] ?></p>
+            </div>
+        </div>
+
+        <div class="W-full h-auto flex flex-col items-center justify-center gap-4 sm:flex-row">
+
+            <?php
+            $buttonLink = "ranking.php";
+            $ariaDescription = "Voir le classement";
+            $textButton = "Voir le classement";
+            require "partials/start_button.php";
+            ?>
+
+            <?php
+            $buttonLink = "choice_quiz.php";
+            $ariaDescription = "Revenir au choix du quiz";
+            $textButton = "Retour aux quiz";
+            require "partials/start_button.php";  ?>
+        </div>
+    </div>
+</main>
+
+<?php
+require_once "partials/footer.php";
+?>
